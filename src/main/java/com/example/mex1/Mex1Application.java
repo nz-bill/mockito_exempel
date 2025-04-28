d0509ad12c5473cd50f5aad4b4aeb30fbc419c5d
nz-bill <bill.palmestedt@zocom.se>
2025-04-28 06:59:04 +0200

    added comment

--- src/main/java/com/example/mex1/Mex1Application.java
+++ src/main/java/com/example/mex1/Mex1Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Mex1Application {
 
+	//add a comment
+
 	public static void main(String[] args) {
 		SpringApplication.run(Mex1Application.class, args);
 	}
